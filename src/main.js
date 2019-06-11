00e0f3bdb9826e0175174f8b0535fbdff5907444
lxandhby <lxandhby@lxandhbydeMacBook-Pro.local>
2019-06-11 21:53:08 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
