42e928e1615eba224defb42400cfcc660753a985
LXhbyxiang <2413528465@qq.com>
2019-06-12 21:35:16 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '../views/entrance/register.vue'
+import Introduce from '../views/entrance/introduce'
 Vue.use(Router);
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'Register',
-        component: Register,
-        meta: {
-            title: '注册会员',
-            auth: false
+    routes: [
+        {
+            path: '/',
+            name: 'Register',
+            component: Register,
+            meta: {
+                title: '注册会员',
+                auth: false
+            }
         },
-    }]
+        {
+            path: '/introduce',
+            name: 'Introduce',
+            component: Introduce,
+        },
+    ]
 })
--- src/views/entrance/introduce.vue
+++ src/views/entrance/introduce.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-container fluid>
+      <v-layout row child-flex wrap>
+        <div>
+          <v-toolbar>
+            <v-btn icon class="hidden-xs-only">
+              <v-icon>arrow_back</v-icon>
+            </v-btn>
+
+            <v-toolbar-title>Title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </div>
+
+      </v-layout>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <style lang="scss" scoped>
+
 </style>
