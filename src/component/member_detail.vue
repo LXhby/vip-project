a0fa56884bdbf0de5f5ea7390aa68f63a9567f8f
lxandhby <lxandhby@lxandhbydeMacBook-Pro.local>
2019-06-16 00:42:30 +0800

    0

--- src/component/member_detail.vue
+++ src/component/member_detail.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="member-detail white--text">
-    <v-layout class="hd" align-center>
-      <v-avatar size="60px" class="fl">
+    <div class="hd">
+      <v-avatar size="60px" class="inline-boclk">
         <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
       </v-avatar>
-      <div class="user-info">
+      <div class="user-info inline-boclk">
         <p>
           王晓文
           <i>总经理</i>
         </p>
         <span>北京拓客云科技有限公司</span>
       </div>
-      <div class="star-cart">
+
+      <div class="star-cart inline-boclk fr">
         <v-avatar color="indigo" size="34px">
           <v-icon dark>fas fa-star</v-icon>
         </v-avatar>
         <span>我的名片</span>
         <i class="iconfont icon-next"></i>
       </div>
+    </div>
+
+    <v-layout row class="list-num">
+      <v-flex xs4>
+        <span>16</span>
+        <p>学友</p>
+      </v-flex>
+      <v-flex xs4>
+        <span>16</span>
+        <p>上课</p>
+      </v-flex>
+      <v-flex xs4>
+        <span>16</span>
+        <p>奖金</p>
+      </v-flex>
     </v-layout>
-    <v-container grid-list-md text-xs-center>
-      <v-layout row class="list-num">
-        <v-flex xs4>
-          <span>16</span>
-          <p>学友</p>
-        </v-flex>
-        <v-flex xs4>
-          <span>16</span>
-          <p>上课</p>
-        </v-flex>
-        <v-flex xs4>
-          <span>16</span>
-          <p>奖金</p>
-        </v-flex>
-      </v-layout>
-    </v-container>
   </div>
 </template>
 
@@ -48,7 +48,9 @@ export default {};
   font-size: 25px;
   padding: 34px;
   background-color: $bg-color;
+  border-radius: 8px;
   .hd {
+    position: relative;
     .user-info {
       margin-left: 20px;
       font-size: 28px;
@@ -61,11 +63,25 @@ export default {};
       }
     }
     .star-cart {
+      margin-top: 34px;
+      margin-right: -34px;
       border-radius: 27px 0 0 27px;
       background: rgba(255, 255, 255, 0.2);
       .v-avatar {
         opacity: 1;
+        .fa-star {
+          &:before {
+            font-size: 25px;
+          }
+        }
       }
+    }
+  }
+  .list-num {
+    padding: 12px 0;
+    margin-top: 60px;
+    p {
+      margin-top: 8px;
     }
   }
 }
