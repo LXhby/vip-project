d66fbc14b7fe64a1dc03e20077975d675b4832e5
LXhbyxiang <2413528465@qq.com>
2019-06-16 11:05:17 +0800

    1

---
+++ src/views/businessarea/business.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="padding-20">
+      <user-detail></user-detail>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "@/component/user_detail";
+export default {
+  components: {
+    UserDetail
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/public.scss";
+
+</style>
