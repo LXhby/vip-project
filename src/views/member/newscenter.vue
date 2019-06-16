ee499243d2a8d11e235eee9315773d67a1b82102
LXhbyxiang <2413528465@qq.com>
2019-06-16 17:13:52 +0800

    1

--- src/views/member/newscenter.vue
+++ src/views/member/newscenter.vue
@@ -3,7 +3,25 @@
     <member-detail></member-detail>
     <div class="main">
       <div class="news-list">
-        <div class="news-nav">
+        <div class="news-head">
+          <div class="news-title">
+            <p></p>
+            <span>消息中心</span>
+          </div>
+          <div class="news-select">
+            <v-menu transition="slide-y-transition" bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn flat small v-on="on"><v-icon>fas fa-bars</v-icon>筛选</v-btn>
+              </template>
+              <v-list>
+                <v-list-tile v-for="(item, i) in items" :key="i" @click>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
+          </div>
+        </div>
+        <div class="news-nav" v-for="(arr,index) in array" :key="index">
           <div class="clearfix">
             <h4 class="fl">【系统消息】</h4>
             <span class="fr">2019.08.31 10:23</span>
@@ -12,14 +30,30 @@
         </div>
       </div>
     </div>
+    <common-bottom></common-bottom>
   </div>
 </template>
 
 <script>
 import MemberDetail from "@/component/user_detail";
+import CommonBottom from "@/component/common_bottom";
 export default {
   components: {
-    MemberDetail
+    MemberDetail,
+    CommonBottom
+  },
+  data() {
+    return {
+      array: ["1", "2", "3", "4"],
+      items: [
+        { title: '未读消息' },
+        { title: '已读消息' },
+        { title: '系统消息' },
+        { title: '开课通知' },
+        { title: '报名通知' },
+        { title: '收益通知' }
+      ]
+    };
   }
 };
 </script>
@@ -29,6 +63,27 @@ export default {
 .news-center-page {
   .main {
     .news-list {
+      .news-head {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 30px 0 0 0;
+        .news-title {
+          display: flex;
+          align-items: center;
+          p {
+            margin-right: 10px;
+            width: 6px;
+            height: 25px;
+            border-radius: 3px;
+            background-color: $bg-color;
+          }
+          span {
+            color: gray;
+            font-size: 30px;
+          }
+        }
+      }
       .news-nav {
         h4 {
           margin-left: -12.5px;
