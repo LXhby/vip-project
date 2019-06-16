38bb9925c6d0f9a1dbb9b57fdea256297b51f420
lxandhby <lxandhby@lxandhbydeMacBook-Pro.local>
2019-06-16 12:45:35 +0800

    0

--- src/views/member/vipcenter.vue
+++ src/views/member/vipcenter.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="padding-20">
     <member-detail></member-detail>
+    <course-list></course-list>
   </div>
 </template>
 
 <script>
 import MemberDetail from "@/component/user_detail";
+import CourseList from "@/component/course_list";
 export default {
   components: {
-    MemberDetail
+    MemberDetail,
+    CourseList
   }
 };
 </script>
