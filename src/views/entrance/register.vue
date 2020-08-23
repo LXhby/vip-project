2feb9a6bfcfe3366dfeab0e7fd677b9abd3305d9
lxandhby <473249753@qq.com>
2020-08-23 17:10:40 +0800

    1

--- src/views/entrance/register.vue
+++ src/views/entrance/register.vue
@@ -157,7 +157,7 @@ export default {
     }
   },
   created() {
-    console.log('created')
+    console.log('created1')
     getmember_order().then(res => {
       console.log("res", res);
       this.list = res.data.items;
